96aa748f3110dc434c576c1578482b5ea2ee231b
Tsutsa <Nathaniel.Tsutsa@standardbank.co.za>
2023-04-25 00:28:35 +0200

    tried _printfC

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@
 /*Return printed character numbers
 *_putchar - writing character c to stdout
 *@c: the charater to print
-*/
+*
 typedef struct custom_printf {
 char *buffer;
 int buffer_size;
@@ -76,4 +76,42 @@ print_char(custom_printf, *str++);
 void print_percent(custom_printf_t *custom_printf)
 {
 print_char(custom_printf, '%');
+}*/
+
+void print_buffer(char buffer[], int *buff_ind);
+/*_printf - PRINTF FUNCTION
+*@format: This is the format call
+*Return: printed chars
+*/
+int _printf(const char *format, ...)
+{int i, printed = 0, printed_char = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0';i++)
+{if (format[i] != '%')
+{buffer[buff_ind++ = format[i];
+if(buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+printed_chars++;
+}
+else
+{print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+if(printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return (printed_chars);
 }

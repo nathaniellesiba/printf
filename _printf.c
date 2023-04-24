850d215a2444b96b408c5d03c6e38cfaff65eaab
Tsutsa <Nathaniel.Tsutsa@standardbank.co.za>
2023-04-24 22:14:57 +0200

    rectified printf

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/*Return printed character numbers
+*_putchar - writing character c to stdout
+*@c: the charater to print
+*/
+{
+va_list args;
+va_start(args, format);
+int count = 0;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+fputs(va_arg(args, char*), stdout);
+count += strlen(va_arg(args, char*));
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return count;
+}

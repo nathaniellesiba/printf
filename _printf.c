1acff5a3e9292649d57471f137b53b3f4468d8a5
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-25 21:04:42 +0200

    printf Test 002

--- _printf.c
+++ _printf.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
@@ -55,72 +54,3 @@ if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
 *buff_ind = 0;
 }
-
-
-
-/**
- * #include <stdio.h>
-#include <stdarg.h>
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-=======
-/*
->>>>>>> 55c81eeca9169f33c2402d973e7a33b0b5c0bef6
-*_printf - PRINTF FUNCTION
-*@format: This is the format call
-*Return: printed chars
-*
-int _printf(const char *format, ...)
-{int i, printed = 0, printed_char = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{buffer[buff_ind++ = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-printed_chars++;
-}
-else
-{print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i,
-list, buffer, flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
-
-<<<<<<< HEAD
-*print_buffer
- * @buffer: char arrays
- * @buff_ind: indexing
- *
-=======
-/*
-*print_buffer
-*@buffer: char arrays
-*@buff_ind: indexing
-*/
->>>>>>> 55c81eeca9169f33c2402d973e7a33b0b5c0bef6
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0
-*/

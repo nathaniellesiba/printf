5a5605ff96f6a21663ab483a2e1bd0ff9a943ed4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-26 01:18:42 +0200

    _printfC submission

--- main.h
+++ main.h
@@ -16,7 +16,7 @@
 #define S_SHORT 1
 /**
 *struct fmt - Struct op
-*
+*@valid: the valid character
 *@fmt: The first format
 * @fn: The function associated
 */
@@ -32,9 +32,20 @@ int (*fn)(va_list, char[], int, int, int, int);
 * @fm_t: The function associated
 */
 typedef struct fmt fmt_t;
+{char *valid;
+int(*f)(va_list);
+}
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+int print c(va_list args);
+int print s(va_list args);
+int print d(va_list args);
+int print i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args)
+
 /****************** FUNCTIONS ******************/
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -42,6 +53,8 @@ int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
+int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
@@ -69,6 +82,16 @@ int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+int print_binary(unasigned int n);
+int main(const char *format, ...)
+{
+unsigned int num=42;
+custom_printf("the binary representation of %u is %b\n", num, num);
+return 0;
+};
+int print_u(unsigned int n, int base, int is_uppercase);
+int print_o(unsigned int n);
+int print_x(unsigned int n, int is_uppercase);
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 int flags, int width, int precision, int size);

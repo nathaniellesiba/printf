ca4f29aa6f01d302d3ea01a1687429c60bfd3d1a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-25 21:12:51 +0200

    printf Test 003

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
-*_printf - Printf function
-*@format: format
-*Return: Printed chars
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
 */
 int _printf(const char *format, ...)
 {
@@ -44,9 +44,9 @@ va_end(list);
 return (printed_chars);
 }
 /**
-* print_buffer - printed buffer content
+* print_buffer - Prints the contents of the buffer if it exist
 * @buffer: Array of chars
-* @buff_ind: Indexing next char
+* @buff_ind: Index at which to add next char, represents the length.
 */
 void print_buffer(char buffer[], int *buff_ind)
 {

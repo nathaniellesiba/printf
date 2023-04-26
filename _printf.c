e2c5221beb1c18cc71ba8dc630f3a297f94fa943
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-26 02:14:59 +0200

    printfC 003

--- _printf.c
+++ _printf.c
@@ -1,103 +1,56 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
-
-/*
-*print - printing characters
-*@args: character args
-*return: number of characters
+void print_buffer(char buffer[], int *buff_ind);
+/**
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
 */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char c, *s;
-va_start(args, format);
-while (*format != '\0') {
-if (*format == '%') {
-format++;
-switch (*format) {
-case 'c':
-c = va_arg(args, int);
-putchar(c);
-count++;
-break;
-case 's':
-s = va_arg(args, char *);
-while (*s != '\0') {
-putchar(*s);
-s++;
-count++;
-}
-break;
-case '%':
-putchar('%');
-count++;
-break;
-}
-} else {
-putchar(*format);
-count++;
-}
-format++;
-}
-va_end(args);
-return count;
-}
-int main(void)
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0'; i++)
 {
-int count;
-count = _printf("Hello, %s! The answer is %d%c.\n, world", 42, '%');
-printf("Number of characters printed: %d\n", count);
-return 0;
-}
-
-/*
-*Custom function to convert
-*unsigned int to binary
-*/
-void int_to_binary(unsigned int num)
+if (format[i] != '%')
 {
-if (num > 1)
-int_to_binary(num / 2);
-printf("%d", num % 2);
-}
-/*
-*Custom printf function
-*to handle b conversion specifier
-*/
-void custom_printf(char* format, ...)
-{
-/*Initialize arguments*/
-va_list arg;
-int i, val;
-char c;
-/*Start variable argument list*/
-va_start(arg, format);
-/*Loop through format string*/
-for (i = 0; format[i] != '\0'; i++)
-{
-/*Check for conversion specifier*/
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
-{
-case 'b':   /*Convert unsigned int to binary*/
-val = va_arg(arg, unsigned int);
-int_to_binary(val);
-break;
-default:
-printf("Error: Unsupported conversion specifier %%%c\n", format[i]);
-break;
-}
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
 }
 else
 {
-/*Print regular character*/
-putchar(format[i]);
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
 }
 }
-/*End variable argument list*/
-va_end(arg);
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return (printed_chars);
+}
+/**
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
 }

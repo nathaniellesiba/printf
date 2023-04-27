41e4c3b0accaf696585f3bb18233b38c2d392470
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-04-27 02:48:56 +0200

    teamProjectII

--- _printf.c
+++ _printf.c
@@ -1,4 +1,9 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
 /**
 *_printf - Printf function
@@ -55,4 +60,51 @@ write(1, &buffer[0], *buff_ind);
 *buff_ind = 0;
 }
 
-/
+
+/**
+* _printf - recreates printf function
+* @format: string with format specifier
+* Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+if (format != NULL)
+{
+int count = 0, i;
+int (*m)(va_list);
+va_list args;
+
+va_start(args, format);
+i = 0;
+if (format[0] == '%' && format[1] == '\0')
+return (-1);
+while (format != NULL && format[i] != '\0')
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '%')
+{
+count += _putchar(format[i]);
+i += 2;
+}
+else
+{
+m = get_func(format[i + 1]);
+if (m)
+count += m(args);
+else
+count = _putchar(format[i]) + _putchar(format[i + 1]);
+i += 2;
+}
+}
+else
+{
+count += _putchar(format[i]);
+i++;
+}
+}
+va_end(args);
+return (count);
+}
+return (-1);
+}
